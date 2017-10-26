0f2a8842c2dec0a8b0866ffb373fcab32097072a
laly64 <avalos53laly@gmail.com>
2017-10-25 21:05:19 -0300

    En este ejercicio se declararon variables

---
+++ src/app1lavinia/App1lavinia.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1lavinia;
+
+/**
+ *
+ * @author LAB02
+ */
+public class App1lavinia
+{
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int numero1;
+          int numero2;
+          
+                  
+          numero1=53;
+          numero2=40;
+           System.out.println("suma de numero1 y numero2: "+(numero1+numero2));
+            System.out.println("resta de numero1 y numero2: "+(numero1-numero2));
+            System.out.println("multiplicar de numero1 y numero2: "+(numero1*numero2));
+            System.out.println("dividir de numero1 y numero2: "+(numero1/numero2));
+            System.out.println("residuo de numero1 y numero2: "+(numero1%numero2));
+    }
+    
+}
